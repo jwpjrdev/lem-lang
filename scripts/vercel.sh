b253e4d13fcc23dd8bd0a82958ff2d2fc1162116
Joshua P <jpric2024@olemiss.k12.in.us>
2022-02-25 22:05:50 +0000

    fix: make vercel.sh cd to the right path

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -1,5 +1,5 @@
 # install dependencies for mdbook to deploy on vercel
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-cd .cargo/bin/
+cd $HOME/.cargo/bin/
 ./cargo install mdbook
