22a64a6abc1ac27e49087ce2f8ad7360b8a0606c
jwpjrdev <jwpjr567@gmail.com>
2022-02-26 17:07:08 -0500

    refactor(build): clean up vercel deployments

--- justfile
+++ justfile
@@ -25,7 +25,3 @@ dev-install:
 # rustup component add rustfmt --toolchain nightly
     cargo install clippy
     cargo install mdbook
-
-vercel-install:
-    mkdir bin
-    curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.15/mdbook-v0.4.15-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=bin
--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -1,4 +1,9 @@
-# install dependencies for mdbook to deploy on vercel
+# Install dependencies for mdbook to deploy on vercel
+
+# This is better than being in the justfile because if this was in there, Vercel would have to
+# 1. install cargo (and by extension rust),
+# 2. compile just,
+# 3. and then finally run these commands
 
 mkdir bin
 curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.15/mdbook-v0.4.15-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=bin
